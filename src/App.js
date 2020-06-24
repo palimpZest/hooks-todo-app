a046f8f36fbdb093aad5326a3708f7287b11735c
palimpZest <polo.vaca365@gmail.com>
2020-06-24 18:43:06 +0200

    fix: Prepare & clean code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <h1>todos</h1> <small>(hooks)</small>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <button>v</button>
+        <input />
+      </div>
     </div>
   );
 }
